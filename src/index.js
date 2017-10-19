f2e9f474c6f2019734aee7a606ab0fc96adee1d5
alphacrash <sudhansu.ace@gmail.com>
2017-10-19 15:35:40 +0530

    Video List

--- src/index.js
+++ src/index.js
@@ -2,23 +2,27 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
 const API_KEY = 'AIzaSyDUe5bcSJoHHD2az4QpwMoExkSUQAOEV1w';
 
-YTSearch({ key: API_KEY, term: 'surfboards' }, function (data) {
-    console.log(data);
-});
+
 
 class App extends Component {
     constructor(props) {
         super(props);
 
         this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos });
+        });
     }
 
     render() {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
