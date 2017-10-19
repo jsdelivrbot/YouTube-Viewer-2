9a370120ffe11830cc7d5aa44eaa1a8521e20192
alphacrash <sudhansu.ace@gmail.com>
2017-10-19 13:45:16 +0530

    Integrating YouTube API Search

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'
-
 const API_KEY = 'AIzaSyDUe5bcSJoHHD2az4QpwMoExkSUQAOEV1w';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>);
+YTSearch({ key: API_KEY, term: 'surfboards' }, function (data) {
+    console.log(data);
+});
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
